64dce53d52a656379cf481fe75c2569892cf9cd2
harshithamanyam <harshithamanyam01@gmail.com>
2026-01-30 20:06:38 +0530

    Initial version of student performance analysis

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter student name: ")
+math = int(input("Math marks: "))
+physics = int(input("Physics marks: "))
+programming = int(input("Programming marks: "))
+average = (math + physics + programming) / 3
+print("\nStudent Name:", name)
+print("Average Marks:", average)
