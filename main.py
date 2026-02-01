016658dd45ceb57a1d9aee63075cafad20d453b8
harshithamanyam <harshithamanyam01@gmail.com>
2026-02-01 20:20:05 +0530

    Added strengths and weaknesses analysis

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 name = input("Enter student name: ")
 
-marks = []
 subjects = ["Math", "Physics", "Programming", "Communication", "Creativity"]
+marks = {}
 
 for subject in subjects:
-    mark = int(input(f"{subject} marks: "))
-    marks.append(mark)
+    marks[subject] = int(input(f"{subject} marks: "))
 
-average = sum(marks) / len(marks)
+strengths = []
+weaknesses = []
+
+for subject, score in marks.items():
+    if score >= 75:
+        strengths.append(subject)
+    else:
+        weaknesses.append(subject)
+
+average = sum(marks.values()) / len(marks)
 
 print("\n--- RESULT ---")
 print("Student Name:", name)
-print("Average Marks:", round(average, 2))
+print("Average:", round(average, 2))
+print("Strengths:", strengths)
+print("Weaknesses:", weaknesses)
 
